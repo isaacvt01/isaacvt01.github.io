a0f4ab208f38a5e8ef5ca9e737c4a68f2a9a92c0
isaacvt01 <_vasqueztucent@cifpfbmoll.eu>
2022-11-30 01:04:08 +0100

    test(test_bicycles_type) Crear test que revise los tipos de bicicletas extraídos

---
+++ test/test_db/test_bicycles_type.py
@@ -0,0 +1,27 @@
+from src.db.get_aero_type import get_aero_type
+from src.db.get_sport_type import get_sport_type
+from src.db.get_trail_type import get_trail_type
+from src.db.get_tipo_triatlon import get_triatlon_type
+from src.db.get_racing_type import get_type_racing
+
+def test_racing():
+    for document in get_type_racing():
+        assert document['bicycle type'] == 'Racing'
+
+def test_triatlon():
+    for document in get_triatlon_type():
+        assert document['bicycle type'] == 'Triatlon'
+
+def test_sport():
+    for document in get_sport_type():
+        assert document['bicycle type'] == 'Sport'
+
+def test_trail():
+    for document in get_trail_type():
+        assert document['bicycle type'] == 'Trail'
+
+def test_aero():
+    for document in get_aero_type():
+        assert document['bicycle type'] == 'Aero'
+
+
