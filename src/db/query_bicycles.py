ca113c3d6278a6875ea4d4e441d61fb32ed7f86a
Manu Ortega <manuomwork@gmail.com>
2022-12-01 18:06:29 +0100

    bug arreglar posición del return

--- src/db/query_bicycles.py
+++ src/db/query_bicycles.py
@@ -14,7 +14,7 @@ def get_bicycles_data():
                 "id": str(document['_id']),
                 "model": document['model'],
                 "bicycle brand": document['bicycle brand'],
-                "image": "/assets/images/bicicleta.webp",
+                "image": "../assets/images/bicicleta.webp",
                 "details_link": "details/" + file_name + ".html",
                 "description": document["description"],
                 "specifications": document["specifications"]
