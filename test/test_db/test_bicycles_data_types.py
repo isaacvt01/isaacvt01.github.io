a8ed9e0c138aaea896d5149d32fb7c79ff455dd9
Manu Ortega <manuomwork@gmail.com>
2022-12-11 19:36:06 +0100

    delete unecesary files

--- test/test_db/test_bicycles_data_types.py
+++
@@ -1,9 +0,0 @@
-from src.db.read.read_bicycles import get_bicycles_data
-
-
-def test_bicycles_list():
-    assert isinstance(get_bicycles_data(), list)
-
-
-def test_bicycles_dict():
-    assert isinstance(get_bicycles_data()[0], dict)
--- test/test_layout/test_data_type_layout.py
+++
@@ -1,7 +0,0 @@
-from src.presentation.view.form_bicycles_view import create_form_page_layout
-from src.presentation.view.main_bicycles_view import create_main_page_layout
-
-
-def test_data_type_layout():
-    assert isinstance(create_form_page_layout(), str)
-    assert isinstance(create_main_page_layout(), str)
