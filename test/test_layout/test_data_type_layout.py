970e6f7ae4fac3633be8e6ab56a7643b8bd40f8d
Manu Ortega <manuomwork@gmail.com>
2022-12-11 19:12:24 +0100

    add git requirements

--- test/test_layout/test_data_type_layout.py
+++ test/test_layout/test_data_type_layout.py
@@ -1,5 +1,5 @@
 from src.presentation.view.form_bicycles_view import create_form_page_layout
-from src.presentation.view import create_main_page_layout
+from src.presentation.view.main_bicycles_view import create_main_page_layout
 
 
 def test_data_type_layout():
