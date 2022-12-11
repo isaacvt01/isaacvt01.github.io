0c347cacda6052d9d5f6e72af448f0ecd09a18e0
Manu Ortega <manuomwork@gmail.com>
2022-12-11 22:04:57 +0100

    adding responsive header

--- src/presentation/view/main_bicycles_view.py
+++ src/presentation/view/main_bicycles_view.py
@@ -18,14 +18,16 @@ def create_main_page_layout():
             <div>
                 <img class="logo" src="/bicycles-project/assets/images/logo2.png" alt="imagen-logo">
             </div>
-                <i class="fa-sharp fa-solid fa-bars"></i>
-            <ul class="nav-menu">
-                <li><a href="../dist/index.html" target="_blank" class="active">Home</a></li>
-                <li><a href="../dist/brands_main.html" target="_blank">Marcas</a></li>
-                <li><a href="../dist/types_main.html" target="_blank">Tipos</a></li>
-                <li><a href="../dist/bicycles.html" target="_blank">Todas las bicicletas</a></li>
-                <li><a href="../dist/form.html">Formulario de registro</a></li>
-            </ul>
+            <div class="nav-container">
+                <ul class="nav-menu">
+                    <li><a href="../dist/index.html" target="_blank" class="active">Home</a></li>
+                    <li><a href="../dist/brands_main.html" target="_blank">Marcas</a></li>
+                    <li><a href="../dist/types_main.html" target="_blank">Tipos</a></li>
+                    <li><a href="../dist/bicycles.html" target="_blank">Todas las bicicletas</a></li>
+                    <li><a href="../dist/form.html">Formulario de registro</a></li>
+                </ul>
+                 <i class="nav-mobile-icon fa-sharp fa-solid fa-bars"></i>
+            </div>
         </nav>
     </header>
 
