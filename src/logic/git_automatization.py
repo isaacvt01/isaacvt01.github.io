f90615340d1f24aa9434f260c6f2d312584f9877
isaacvt01 <_vasqueztucent@cifpfbmoll.eu>
2022-12-14 13:01:52 +0100

    chore Update Web Page data

--- src/logic/git_automatization.py
+++ src/logic/git_automatization.py
@@ -3,9 +3,9 @@ import subprocess
 def git_add():
 
     try:
-        comando_add = f"git add ."
-        comando_subprocess=subprocess.run(comando_add, shell=True, check=True)
-        return comando_subprocess.returncode
+        comand_add = f"git add ."
+        comand_subprocess=subprocess.run(comand_add, shell=True, check=True)
+        return comand_subprocess.returncode
 
     except subprocess.SubprocessError:
         print('El comando git add no ha sido ejecutado correctamente.')
@@ -13,9 +13,9 @@ def git_add():
 def git_commit():
 
     try:
-        comando_commit = f'git commit -m "chore Update Web Page data"'
-        comando_subprocess = subprocess.run(comando_commit, shell=True, check=True)
-        return comando_subprocess.returncode
+        comand_commit = f'git commit -m "chore Update Web Page data"'
+        comand_subprocess = subprocess.run(comand_commit, shell=True, check=True)
+        return comand_subprocess.returncode
 
     except subprocess.SubprocessError:
         print('El comando commit no ha sido ejecutado correctamente.')
@@ -23,9 +23,9 @@ def git_commit():
 
 def git_push():
     try:
-        comando_push = f"git push"
-        comando_subprocess = subprocess.run(comando_push, shell=True, check=True)
-        return comando_subprocess.returncode
+        comand_push = f"git push"
+        comand_subprocess = subprocess.run(comand_push, shell=True, check=True)
+        return comand_subprocess.returncode
 
     except subprocess.SubprocessError:
         print('El comando commit no ha sido ejecutado correctamente.')
